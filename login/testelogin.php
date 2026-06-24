121b7a761206ff6627de77fb0ba029f47c70561f
Eduardo Baumart <eduhsouza.baumt@gmail.com>
2026-06-24 16:46:41 -0300

    tentativa de validação para login

--- login/testelogin.php
+++ login/testelogin.php
@@ -1,18 +1,21 @@
 <?php
-    //16:30
-    if(isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['pass'])){
-
-    include_once('db.php');
-    $email = $_POST['email'];
-    $password = $_POST['pass'];
     
-    $sql = "SELECT * FROM users WHERE email = ':email' and pass = ':pass'";
-    $stmt = $pdo->prepare('$sql');
-    $stmt->execute([
-        ':email' => $email,
-        ':pass' => $pass
-    ]);
-    echo $stmt;
-    } else {
-        header('location: login.php');
-    }
+    try {
+        include_once('db.php');
+        $email = $_POST['email'];
+        $password = $_POST['pass'];
+
+        if(isset($_POST['login']) && !empty($_POST['email']) && !empty($_POST['pass'])){
+
+            $sql = "SELECT * FROM users WHERE email = ':email' and pass = ':pass'";
+            $stmt = $pdo->prepare('$sql');
+            $stmt->execute([
+                ':email' => $email,
+                ':pass' => $pass
+            ]);
+
+            echo 'Você entrou';
+        }
+    } catch (PDOException $e) {
+        echo 'Erro no login'. $e->getMessage();
+        }
